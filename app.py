bac33c707e4544c019d6607961989432f5d78740
OJEELE <120004515+OJEELE@users.noreply.github.com>
2024-01-08 23:07:19 +0000

    created html page and added images

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "hello world"
-  #render_template('index.html')
+    return render_template('index.html')
   
 if(__name__ == "__main__" ):
     app.run(host = "0.0.0.0", debug = True)
